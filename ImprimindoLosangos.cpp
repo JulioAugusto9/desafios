474748237e70ffd66d5cb3ffdf1438c29a03646d
JulioAugusto9 <56029778+JulioAugusto9@users.noreply.github.com>
2020-11-11 08:36:02 -0300

    Create ImprimindoLosangos.cpp

---
+++ ImprimindoLosangos.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void print(int men, int mai)
+{
+    for (int i=0; i<=mai; i++)
+    {
+        if (i==men || i==mai)
+            cout<<'#';
+        else
+            cout<<' ';
+        }
+        cout << endl;
+}
+
+bool losango(int men, int mai)
+{
+    if (men<0) return false;
+        print(men, mai);
+    if (losango(men-1, mai+1))
+    {
+        print(men, mai);
+    }
+    return true;
+}
+
+int main()
+{
+    for (int n=1; n<=1000; n++)
+    {
+        losango(n-1, n-1);
+    }
+    return 0;
+}
+
+//Desafio do @geek2code (instagram)
